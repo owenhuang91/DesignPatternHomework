95e287b0ffcdb004fadbbf0971e8cf44a6ba51e5
Owen <owen2862@live.com>
2018-08-12 12:47:44 +0800

    refactor

--- UnitTestProject2/TaxHelper.cs
+++ UnitTestProject2/TaxHelper.cs
@@ -1,22 +1,39 @@
-﻿namespace UnitTestProject2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject2
 {
     public class TaxHelper
     {
-        public decimal GetTaxResult(decimal tax)
+        private Dictionary<int, decimal> _percentLookup = new Dictionary<int, decimal>
         {
-            if (tax > 2420000)
+            {540000,0.05m},
+            {1210000,0.12m},
+            {2420000,0.2m},
+            {Int32.MaxValue,0.3m},
+        };
+
+        public decimal GetTaxResult(decimal income)
+        {
+            var maxLevel = _percentLookup.FirstOrDefault(m => m.Key >= income);
+            return maxLevel.Key == _percentLookup.FirstOrDefault().Key
+                ? maxLevel.Value * income
+                : maxLevel.Value * maxLevel.Key - income + GetTaxResult(maxLevel.Key);
+
+            if (income > 2420000)
             {
-                return 540000 * 0.05m + (1210000 - 540000) * 0.12m + (2420000 - 1210000) * 0.2m + (tax - 2420000) * 0.3m;
+                return 540000 * 0.05m + (1210000 - 540000) * 0.12m + (2420000 - 1210000) * 0.2m + (income - 2420000) * 0.3m;
             }
-            if (tax > 1210000)
+            if (income > 1210000)
             {
-                return 540000 * 0.05m + (1210000 - 540000) * 0.12m + (tax - 1210000) * 0.2m;
+                return 540000 * 0.05m + (1210000 - 540000) * 0.12m + (income - 1210000) * 0.2m;
             }
-            if (tax > 540000)
+            if (income > 540000)
             {
-                return 540000 * 0.05m + (tax - 540000) * 0.12m;
+                return 540000 * 0.05m + (income - 540000) * 0.12m;
             }
-            return tax * 0.05m;
+            return income * 0.05m;
         }
     }
 }
